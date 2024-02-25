ab563ef65c08dd04b364d113f4ce6fecb10156a9
0xRy4n <ry4n@hackthebox.eu>
2024-02-24 22:19:44 -0500

    Switch driver to chrome

--- airistotle/plugins/url_viewer.py
+++ airistotle/plugins/url_viewer.py
@@ -5,9 +5,10 @@ import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 from .base import BasePlugin
 
@@ -19,14 +20,13 @@ class UrlViewer(BasePlugin):
     def run(self, *args, **kwargs) -> str:
         url = kwargs["url"]
         options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        driver = Firefox(options=options)
-
-        time.sleep(3)
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         driver.get(url)
         text = html2text.html2text(driver.page_source)
 
         driver.quit()
-        
+
         return text or "Could not retrieve content from the URL."
